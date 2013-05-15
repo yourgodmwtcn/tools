85b26fa0f855bc2f5a2f8ad56009a405c3939bf7
deepak <dpak.cherian@gmail.com>
2013-05-15 11:48:07 -0400

    Added grep.
    Updated read_params_from_ini  to accept directory input. It scans for ININAME and opens appropriate initial conditions file.
    
    Signed-off-by: deepak <dpak.cherian@gmail.com>

--- dc/ROMS/read_params_from_ini.m
+++ dc/ROMS/read_params_from_ini.m
@@ -1,7 +1,16 @@
 % [params] = read_params_from_ini(fname)
+% Pass either *.in file or directory
 
 function [params] = read_params_from_ini(fname)
 
+    if isdir(fname)
+        if isempty(strfind(fname,'config')), fname = [fname '/config/']; end
+        in = ls([fname '/*.in']);
+        [~,p] = grep('ININAME == ',[fname in]); 
+        % line in p.match must be processed to extract *.nc name
+        A = sscanf(char(p.match),' ININAME == %s');
+        fname = [fname '/' A];
+    end
     info = ncinfo(fname);
     n = length(info.Variables);
     
