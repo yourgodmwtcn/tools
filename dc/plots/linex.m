72551edb36131c325ad38c7bbb02b2786c894bc7
deepak <dpak.cherian@gmail.com>
2013-09-30 14:27:48 -0400

    updated linex, liney to use backend dcline

--- dc/plots/linex.m
+++ dc/plots/linex.m
@@ -3,31 +3,7 @@
 
 function [handles] = linex(x,label,color)
     
-    hFig = evalin('caller','gcf');
-    hAxis = evalin('caller','gca');
-    
-    if size(x,2) == 1, x = x'; end
-    
-    figure(hFig);
-    hold on;
-    
     if ~exist('label','var'), label = num2str(x'); end
     if ~exist('color','var'), color = 'k'; end
     
-    yax = (get(hAxis,'YLim'));
-    xtick = get(hAxis,'XTick');
-    %xax = (get(hAxis,'XLim'));
-    
-    %loglog([x; x]',repmat(yax,length(x),1),'k-','LineWidth',1.5);
-    
-    for i=1:length(x)       
-        handles(i) = plot([x(i) x(i)],yax,'--','LineWidth',1.5,'Color',color);
-%        set(hAxis,'XTick',sort([xtick x(i)]));
-        %if mod(i,2)
-        if ~isempty(label)
-            text(double(x(i)),double(yax(end)-(length(label))/2),label,'Rotation',0,'VerticalAlignment','Bottom','FontSize',10,'Color',color);%,'FontWeight','Bold');
-        end
-        %else
-        %    text(x(i),yax(mod(i,2)+2),num2str(1./factor./x(i)),'Rotation',90,'VerticalAlignment','Bottom','FontWeight','Bold');
-        %end
-    end
+    handles = dcline('x',x,label,color);
--- dc/plots/liney.m
+++ dc/plots/liney.m
@@ -3,27 +3,6 @@
 
 function [handles] = liney(y,label,color)
     
-    hFig = evalin('caller','gcf');
-    hAxis = evalin('caller','gca');
-    
-    figure(hFig);
-    hold on;
-    
-    if ~exist('label','var') || isempty(label), label = ' '; end
+    if ~exist('label','var'), label = num2str(y'); end
     if ~exist('color','var'), color = 'k'; end
-    
-    %yax = (get(hAxis,'YLim'));
-    xax = (get(hAxis,'XLim'));
-    ytick = get(hAxis,'YTick');    
-    
-    %loglog([x; x]',repmat(yax,length(x),1),'k-','LineWidth',1.5);
-    
-    for i=1:length(y)       
-        handles(i) = plot(xax,[y(i) y(i)],'--','LineWidth',1.5,'Color',color);
-%        set(hAxis,'YTick',sort([ytick y(i)]));
-        %if mod(i,2)
-            text(double(xax(end)-(length(label)))/2,double(y(i)),label,'Rotation',0,'VerticalAlignment','Bottom','FontSize',10,'Color',color);%,'FontWeight','Bold');
-        %else
-        %    text(x(i),yax(mod(i,2)+2),num2str(1./factor./x(i)),'Rotation',90,'VerticalAlignment','Bottom','FontWeight','Bold');
-        %end
-    end
+    handles = dcline('y',y,label,color);
