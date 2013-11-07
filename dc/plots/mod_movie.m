f64f05ae4361f495f64558bca6a4e46fe98eb84f
deepak <dpak.cherian@gmail.com>
2013-11-06 19:41:56 -0500

    fixed bugs in mod_movie

--- dc/plots/mod_movie.m
+++ dc/plots/mod_movie.m
@@ -195,6 +195,15 @@ else
     end
 end
 
+% check if there is topography
+if bsxfun(@minus,zax,zax(1,1,:)) == zeros(size(zax))
+    warning('no topography detected.');
+    flags.notopo = 1;
+else
+    flags.notopo = 0;
+end
+            
+
 %% Plot according to options
 
 labels.tmax = time(tindices(1) + dt*floor((tindices(2)-tindices(1))/dt));
@@ -212,12 +221,12 @@ end
 % if ~isDir, figure; end
 
 % given location instead of index
-if axis ~= 'z'
+if axis == 'z' && flags.notopo == 0
+    if ischar(index),index = abs(str2double(index)); end
+else
     if strcmpi(index,'end') || strcmpi(index,'inf'),  index = vinfo.Size(axind); end
     if strcmpi(index,'mid'), index = num2str((sliceax(1)+sliceax(end))/2); end
     if ischar(index), index = find_approx(sliceax,str2double(index),1); end
-else
-    index = abs(str2double(index));
 end
 
 %if ~isempty(strfind(labels.yax,'degree')) || ~isempty(strfind(labels.xax,'degree'))
@@ -247,9 +256,9 @@ for i=0:iend-1
     if i>0 && strcmp(get(gcf,'currentkey'),'escape')
         return;
     end
-    
+
     [read_start,read_count] = roms_ncread_params(dim,i,iend,slab,tindices,dt,vol);
-    
+
     labels.time = time(read_start(end):dt:(read_start(end)+(read_count(end))*dt -1)); % read_start(end)-1
     labels.stride = i;
 
@@ -258,90 +267,91 @@ for i=0:iend-1
         read_start(axind) = index;
         read_count(axind) = 1;
     end
-    
+
     if axis ~= 'z' || dim == 3
         dv = double(squeeze(ncread(fname,varname,read_start,read_count,stride)));  
     else
+        % first check if there's topo
+        if flags.notopo
+            dv = double(squeeze(ncread(fname,varname,read_start,read_count,stride))); 
+        else % we need to interpolate
+            grids.xax = xax;
+            grids.yax = yax;
+            grids.zax = zax;
 
+            warning off
+            read_start(3) = 1;
+            read_count(3) = Inf;
+            for mmm = 1:read_count(4)
+                disp(['reading & interpolating timestep ' num2str(mmm) '/' num2str(read_count(4))]);
+                data = squeeze(double(ncread(fname,varname, ...
+                                [read_start(1:3) read_start(4)+mmm-1], ...
+                                [read_count(1:3) 1],stride)));
 
-        grids.xax = xax;
-        grids.yax = yax;
-        grids.zax = zax;
-
-        warning off
-        read_start(3) = 1;
-        read_count(3) = Inf;
-        for mmm = 1:read_count(4)
-            disp(['reading & interpolating timestep ' num2str(mmm) '/' num2str(read_count(4))]);
-            data = squeeze(double(ncread(fname,varname, ...
-                            [read_start(1:3) read_start(4)+mmm-1], ...
-                            [read_count(1:3) 1],stride)));
-
-            dv(:,:,mmm) = dc_roms_zslice_var(data,index,grids);
-        end
-        clear data
-        warning on
-        % pause since this takes a long time and I might go do something
-        % else
-        commands = [commands; 'pause'];
+                dv(:,:,mmm) = dc_roms_zslice_var(data,index,grids);
+            end
+            clear data
+            warning on
+            % pause since this takes a long time and I might go do something
+            % else
+            commands = [commands; 'pause'];
             [dv(:,:,mmm),~,~] = roms_zslice_var(permute(data,[3 2 1]),NaN,index,grd);
-        end
-        dv = permute(dv,[2 1 3]);
-        warning on
-    end
-    
-    % take care of walls for mitgcm - NEEDS TO BE CHECKED
-    if gcm
-        s = size(dv);
-        s(3) = size(dv,3); % correct for single timestep snapshots - in that case s is a 2 element row
-        if repnan(dv(1,:,:),0)   == zeros([1 s(2) s(3)]), dv(1,:,:) = NaN; end;
-        if repnan(dv(end,:,:),0) == zeros([1 s(2) s(3)]), dv(end,:,:) = NaN; end;
-        
-        if axind == 3
-            if repnan(dv(:,1,:),0)   == zeros([s(1) 1 s(3)]), dv(:,1,:)   = NaN; end;
-            if repnan(dv(:,end,:),0) == zeros([s(1) 1 s(3)]), dv(:,end,:) = NaN; end;
+            dv = permute(dv,[2 1 3]);
         end
     end
-    
-    s = size(dv);            
-    if s(1) == 1 || s(2) == 1
-        close(gcf);
-        error('2D simulation?');
-    end
-    
-    if max(plotx(:)) > 1000
-        plotx = plotx/1000;
-        labels.xax = [labels.xax ' x 10^3'];
-    end
-    if max(ploty(:)) > 1000
-        ploty = ploty/1000;
-        labels.yax = [labels.yax ' x 10^3'];
-    end
-    
-    % fix axes
-    if dim == 3, index = 1; end
-    switch axind
-        case 1
-            plotx = squeeze(plotx(index,:,:,:));
-            ploty = squeeze(ploty(index,:,:,:));
-            
-        case 2
-            plotx = squeeze(plotx(:,index,:,:));
-            ploty = squeeze(ploty(:,index,:,:));    
-        case 3
-            plotx = squeeze(plotx(:,:,1,:));
-            ploty = squeeze(ploty(:,:,1,:));
-    end
+    warning on
+end
 
-    % send to animate
-    torepeat = 1;
-    while torepeat
-        [labels.mm_instance,h_plot] = animate(plotx,ploty,dv,labels,commands,3);
-        torepeat = input('Repeat? (1/0): ');
-    end
+% take care of walls for mitgcm - NEEDS TO BE CHECKED
+if gcm
+    s = size(dv);
+    s(3) = size(dv,3); % correct for single timestep snapshots - in that case s is a 2 element row
+    if repnan(dv(1,:,:),0)   == zeros([1 s(2) s(3)]), dv(1,:,:) = NaN; end;
+    if repnan(dv(end,:,:),0) == zeros([1 s(2) s(3)]), dv(end,:,:) = NaN; end;
 
+    if axind == 3
+        if repnan(dv(:,1,:),0)   == zeros([s(1) 1 s(3)]), dv(:,1,:)   = NaN; end;
+        if repnan(dv(:,end,:),0) == zeros([s(1) 1 s(3)]), dv(:,end,:) = NaN; end;
+    end
+end
+
+s = size(dv);            
+if s(1) == 1 || s(2) == 1
+    close(gcf);
+    error('2D simulation?');
+end
+
+if max(plotx(:)) > 1000
+    plotx = plotx/1000;
+    labels.xax = [labels.xax ' x 10^3'];
+end
+if max(ploty(:)) > 1000
+    ploty = ploty/1000;
+    labels.yax = [labels.yax ' x 10^3'];
+end
+
+% fix axes
+if dim == 3, index = 1; end
+switch axind
+    case 1
+        plotx = squeeze(plotx(index,:,:,:));
+        ploty = squeeze(ploty(index,:,:,:));
+
+    case 2
+        plotx = squeeze(plotx(:,index,:,:));
+        ploty = squeeze(ploty(:,index,:,:));    
+    case 3
+        plotx = squeeze(plotx(:,:,1,:));
+        ploty = squeeze(ploty(:,:,1,:));
+end
+
+% send to animate
+torepeat = 1;
+while torepeat == 1,
     [labels.mm_instance,h_plot] = animate(plotx,ploty,dv,labels,commands,3);
-    
-    % for movie
-    if ~isempty(labels.mm_instance), mm_render(labels.mm_instance); end
+    torepeat = input('Repeat? (1/0): ');
+end
+
+% for movie
+if ~isempty(labels.mm_instance), mm_render(labels.mm_instance); end
 end
